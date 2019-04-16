ef27c18945e13a3a9816cbed567c68d8ec36bea9
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2019-04-16 21:08:42 +0200

    Initial Commit

---
+++ Restaurants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurants
+//
+//  Created by Andrii Kuzminskyi on 16/04/2019.
+//  Copyright © 2019 akuzminskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RestaurantsTests/RestaurantsTests.swift
@@ -0,0 +1,34 @@
+//
+//  RestaurantsTests.swift
+//  RestaurantsTests
+//
+//  Created by Andrii Kuzminskyi on 16/04/2019.
+//  Copyright © 2019 akuzminskyi. All rights reserved.
+//
+
+import XCTest
+@testable import Restaurants
+
+class RestaurantsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
